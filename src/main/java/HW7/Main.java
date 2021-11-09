2c7a0357229bea190bf15244b715f9b49aa7a062
NadezdhaDekhand <nadeshka7979@gmail.com>
2021-11-09 21:17:13 +0700

    HW7

---
+++ src/main/java/HW7/Main.java
@@ -0,0 +1,16 @@
+package HW7;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String cityCode = RequestHandler.detectCityId("Novosibirsk");
+        System.out.println(cityCode);
+        String forecast = RequestHandler.getForecast(cityCode);
+        System.out.println(forecast);
+        String oneDay = RequestHandler.getOneDay(cityCode);
+        System.out.println(oneDay);
+        System.out.println();
+
+    }
+}
